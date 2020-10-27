f4144c3d0753ee41145d01b461721d1e084e5bf9
Chigusa Kobayashi <ckobayahi@riken.jp>
2020-10-27 16:50:50 +0900

    Update recipe of GENESIS for newer versions

--- var/spack/repos/builtin/packages/genesis/package.py
+++ var/spack/repos/builtin/packages/genesis/package.py
@@ -14,11 +14,28 @@ class Genesis(AutotoolsPackage):
     """
 
     homepage = "https://www.r-ccs.riken.jp/labs/cbrt/"
-    url      = "https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2019/10/genesis-1.4.0.tar.bz2"
+#    url = "https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2020/09/genesis-1.5.1.tar.bz2"
 
-    version('1.4.0', '132d53e7edbd593d67ec53cd4618748a')
+    version('2.0b',
+            git='https://github.com/genesis-release-r-ccs/genesis-2.0.git')
+    version('1.5.1','28d696e681efd76cc8a875dc0b305794',
+            url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2020/09/genesis-1.5.1.tar.bz2')
+    version('1.5.0','385e35430da2b42a6a9f1961a6345be3',
+            url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2020/05/genesis-1.5.0.tar.bz2')
+    version('1.4.0', '132d53e7edbd593d67ec53cd4618748a',
+            url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2019/10/genesis-1.4.0.tar.bz2')
+    version('1.3.0','19dc4249af1472d75a110d157364b610',
+            url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2018/06/genesis-1.3.0.tar.bz2')
+    version('1.2.1','b40b05e327679fac6d1c20208577ea40',
+            url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2018/02/genesis-1.2.1.tar.bz2')
+    version('1.2.0','47670beca36398eaae8b3fc72e0a9dcd',
+            url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2018/01/genesis-1.2.0.tar.bz2')
+    version('1.1.6','2726adf527ab66ffdc2903c0f18c466f',
+             url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2017/10/genesis-1.1.6.tar.bz2')
+    version('1.1.5','827737354f1c0a265b7d9af7bb9bca75',
+             url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2017/07/genesis-1.1.5.tar.bz2')
 
-    resource(when='@1.4.0',
+    resource(when='@1.4.0:1.5.1',
              name='user_guide',
              url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2019/10/GENESIS-1.4.0.pdf',
              sha256='da2c3f8bfa1e93adb992d3cfce09fb45d8d447a94f9a4f884ac834ea7279b9c7',
@@ -32,6 +49,7 @@ class Genesis(AutotoolsPackage):
 
     conflicts('%clang', when='+openmp')
     conflicts('~openmp', when='@:1.4.0')
+    conflicts('+fugaku', when='@1.5.0:', msg='Fugaku option is not available.')
 
     depends_on('mpi', type=('build', 'run'))
     depends_on('cuda', when='+gpu')
@@ -51,6 +69,10 @@ class Genesis(AutotoolsPackage):
         if '+gpu' in spec:
             options.append('--enable-gpu')
             options.append('--with-cuda=%s' % spec['cuda'].prefix)
+        if '+debug' in spec:
+            options.append('--enable-debug=3')
+        if '+fugaku' in spec:
+            options.append('--host=Fugaku')
         return options
 
     def configure(self, spec, prefix):
